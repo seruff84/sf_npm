fb9c4db34181b60e1b29b8d41c73b10e1650d677
Dmitrii <dr_d@inbox.ru>
2021-08-16 13:46:11 +0300

    test site

---
+++ static/js/sf-test.js
@@ -0,0 +1,19 @@
+function createTable(tableData) {
+  var table = document.createElement('table');
+  var tableBody = document.createElement('tbody');
+
+  tableData.forEach(function(rowData) {
+    var row = document.createElement('tr');
+
+    rowData.forEach(function(cellData) {
+      var cell = document.createElement('td');
+      cell.appendChild(document.createTextNode(cellData));
+      row.appendChild(cell);
+    });
+
+    tableBody.appendChild(row);
+  });
+
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
+}
